fe4d4dbcb61c41c7322d3d6af33d0f4eb452fd5e
yyhhenry <yyhhenry@foxmail.com>
2023-08-12 14:57:22 +0800

    feat: export

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import CenterLayout from '@/components/CenterLayout.vue';
-import FlexBox from '@/components/FlexBox.vue';
-import FlexCard from '@/components/FlexCard.vue';
-import HeaderText from '@/components/HeaderText.vue';
-import LRMenu from '@/components/LRMenu.vue';
-import MiddleLayout from '@/components/MiddleLayout.vue';
-import PageLayout from '@/components/PageLayout.vue';
-import SwitchDark from '@/components/SwitchDark.vue';
+import CenterLayout from './src/components/CenterLayout.vue';
+import FlexBox from './src/components/FlexBox.vue';
+import FlexCard from './src/components/FlexCard.vue';
+import HeaderText from './src/components/HeaderText.vue';
+import LRMenu from './src/components/LRMenu.vue';
+import MiddleLayout from './src/components/MiddleLayout.vue';
+import PageLayout from './src/components/PageLayout.vue';
+import SwitchDark from './src/components/SwitchDark.vue';
 export {
   CenterLayout,
   FlexBox,
