37fe6e9bf6e71480ac9a05b7a4cd71000ad7ed04
yyhhenry <yyhhenry@foxmail.com>
2023-08-12 12:29:53 +0800

    to: exports

---
+++ index.ts
@@ -0,0 +1,18 @@
+import CenterLayout from '@/components/CenterLayout.vue';
+import FlexBox from '@/components/FlexBox.vue';
+import FlexCard from '@/components/FlexCard.vue';
+import HeaderText from '@/components/HeaderText.vue';
+import LRMenu from '@/components/LRMenu.vue';
+import MiddleLayout from '@/components/MiddleLayout.vue';
+import PageLayout from '@/components/PageLayout.vue';
+import SwitchDark from '@/components/SwitchDark.vue';
+export {
+  CenterLayout,
+  FlexBox,
+  FlexCard,
+  HeaderText,
+  LRMenu,
+  MiddleLayout,
+  PageLayout,
+  SwitchDark,
+};
